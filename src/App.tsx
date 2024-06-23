f8f2f2ccddd253d42f2486004af39ccdf4f97a9f
gabyballester <gaby.ballester@gmail.com>
2024-06-23 12:32:44 +0200

    Basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-function App() {
+import { Box } from '@chakra-ui/react';
+
+const App: React.FC = () => {
   return (
-    <div>
-      <h1>Weather App</h1>
-      <p>Description</p>
-    </div>
+    <Box h="100vh" w="100%">
+      <Box h="100px" bg="blue.500">
+        Header
+      </Box>
+
+      <Box h={`calc(100vh - 100px)`} overflowY="auto">
+        Contenido
+      </Box>
+    </Box>
   );
-}
+};
 
 export default App;
