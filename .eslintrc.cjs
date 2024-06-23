ef36835587203ea09d9ca9fb3be4ad50158f2504
gabyballester <gaby.ballester@gmail.com>
2024-06-23 11:38:19 +0200

    Setup eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,47 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    "plugin:react/jsx-runtime"
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.js', "__mocks__/svgMock.js"],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    'react',
+    '@typescript-eslint',
+    'jsx-a11y',
+    'import',
+    'prettier',
+  ],
   rules: {
+    "react/react-in-jsx-scope": "off",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'prettier/prettier': 'error'
+  },
+  parserOptions: {
+    project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
+
